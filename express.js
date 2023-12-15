c4c1a00feb7ea6a0b0d8e5c4dc34ec5ae630aa12
Abhishek <abhichalana099@gmail.com>
2023-12-15 17:44:59 +0530

    dynamincally add data through tamplate enjine ejs

--- express.js
+++ express.js
@@ -4,21 +4,33 @@ const app = express();
 
 const publicPath = path.join(__dirname, "public");
 
-console.log(publicPath)
+console.log(publicPath);
 
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
 
+app.set("view engine", "ejs");
 
-// app.get("", (req, res) => {
-//     res.send("this is my homePage");
-// });
-// app.get("/about", (req, res) => {
-//     res.send("this is my about");
-// });
-// app.get("/help", (req, res) => {
-//     res.send("this is my help");
-// });
+app.get("", (req, res) => {
+    res.sendFile(`${publicPath}/index.html`);
+});
+app.get("/profile", (req, res) => {
+    const user = {
+        name : "Abhishek",
+        city : "Abohar",
+        phone: 9791999236,
+        skills: ["nodejs", "php", "reactjs", "java", "angular" , "javascript"]
+    }
+    res.render("profile", {user});
+});
 
+app.get("/about", (req, res) => {
+    res.sendFile(`${publicPath}/about.html`);
+});
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+app.get("*", (req, res) => {
+    res.sendFile(`${publicPath}/help.html`);
+});
 
-
-app.listen(5000);
+app.listen(4000);
